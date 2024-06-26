dc5befd4680ed7907ba612c6e241acb503171604
hjw <2232285004@qq.com>
2024-06-26 23:02:27 +0800

    刷题

--- src/main/java/leetcode/editor/cn/[142]环形链表 II.java
+++ src/main/java/leetcode/editor/cn/[142]环形链表 II.java
@@ -107,5 +107,26 @@ public class Solution {
         }
         return null;
     }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while(p2 != null && p2.next != null)
+        {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2)
+            {
+                p2 = head;
+                while (p1 != p2)
+                {
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                return p1;
+            }
+        }
+        return null;
+    }
 }
 // leetcode submit region end(Prohibit modification and deletion)
