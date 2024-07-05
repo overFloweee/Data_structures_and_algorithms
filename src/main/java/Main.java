d8af5ee78298f35c9c169f47f2d9ab75e9398acb
hjw <2232285004@qq.com>
2024-07-05 23:55:56 +0800

    刷题

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        String str = sc.next();
-        int n = str.length();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = n - k; i < n; ++i) {
-            sb.append(str.charAt(i));
-        }
-
-        for (int i = 0; i < n - k; ++i) {
-            sb.append(str.charAt(i));
-        }
-
-        System.out.println(sb);
+        System.out.println(k);
     }
-
 
 }
