7e1981968506eeea602411f50072c3b30a9228e7
hjw <2232285004@qq.com>
2024-07-20 23:33:46 +0800

    刷题

--- src/main/java/Solution1.java
+++ src/main/java/Solution1.java
@@ -1,14 +1,22 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution1 {
 
     public static void main(String[] args) {
+        int[][] people = new int[3][3];
+        Arrays.sort(people, (n1, n2) -> {
+            if (n1[1] != n2[1]) {
+                return n1[1] > n2[1];
+            }
+
+            return n1[0] > n2[0];
+        });
         // System.out.println(new Solution1().largestSumAfterKNegations(new int[]{4, 2, 3}, 1));
         System.out.println(new Solution1().largestSumAfterKNegations(new int[]{2, -3, -1, 5, -4}, 2));
     }
 
     public int largestSumAfterKNegations(int[] nums, int k) {
-        Arrays.sort(nums);
         int sum = 0;
         for (int i = 0; i < nums.length; ++i) {
             int num = nums[i];
