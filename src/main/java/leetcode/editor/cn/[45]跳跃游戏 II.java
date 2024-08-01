d7d8580cf34636d0b7a6fde977c518a25aba85cc
hjw <2232285004@qq.com>
2024-08-01 23:18:19 +0800

    刷题

--- src/main/java/leetcode/editor/cn/[45]跳跃游戏 II.java
+++ src/main/java/leetcode/editor/cn/[45]跳跃游戏 II.java
@@ -1,49 +1,24 @@
-//给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。 
-//
-// 每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处: 
-//
-// 
-// 0 <= j <= nums[i] 
-// i + j < n 
-// 
-//
-// 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。 
-//
-// 
-//
-// 示例 1: 
-//
-// 
-//输入: nums = [2,3,1,1,4]
-//输出: 2
-//解释: 跳到最后一个位置的最小跳跃数是 2。
-//     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
-// 
-//
-// 示例 2: 
-//
-// 
-//输入: nums = [2,3,0,1,4]
-//输出: 2
-// 
-//
-// 
-//
-// 提示: 
-//
-// 
-// 1 <= nums.length <= 10⁴ 
-// 0 <= nums[i] <= 1000 
-// 题目保证可以到达 nums[n-1] 
-// 
-//
-// Related Topics 贪心 数组 动态规划 👍 2553 👎 0
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int jump(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
 
+        int preDistance = 0;
+        int curtDistance = 0;
+        int time = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            curtDistance = Integer.max(curtDistance, i + nums[i]);
+            if (i == preDistance) {
+                preDistance = curtDistance;
+                time++;
+                if (curtDistance + 1 >= nums.length) {
+                    break;
+                }
+
+            }
+        }
+
+        return time;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
--- src/main/java/leetcode/editor/cn/[738]单调递增的数字.java
+++ src/main/java/leetcode/editor/cn/[738]单调递增的数字.java
@@ -1,22 +1,42 @@
 class Solution {
-    public int monotoneIncreasingDigits(int n) {
+    public int monotoneIncreasingDigits1(int n) {
         String str = String.valueOf(n);
         char[] arr = str.toCharArray();
 
         int len = arr.length;
         int index = n;
-        for(int i = len - 2; i >= 0; --i) {
+        for (int i = len - 2; i >= 0; --i) {
             if (arr[i] > arr[i + 1]) {
                 arr[i]--;
-                index  = i + 1;
+                index = i + 1;
             }
         }
 
-        for (int i = index; i < n; ++i) {
+        for (int i = index + 1; i < n; ++i) {
             arr[i] = '9';
         }
 
         str = new String(arr);
         return Integer.valueOf(str);
+    }
+
+    // 二刷
+    public int monotoneIncreasingDigits(int n) {
+
+        char[] arr = String.valueOf(n).toCharArray();
+
+        int index = arr.length - 1;
+        for (int i = arr.length - 2; i >= 0; --i) {
+            if (arr[i] > arr[i + 1]) {
+                index = i;
+                arr[i]--;
+            }
+        }
+
+        for (int i = index + 1; i < arr.length; ++i) {
+            arr[i] = '9';
+        }
+
+        return Integer.valueOf(new String(arr));
     }
 }
