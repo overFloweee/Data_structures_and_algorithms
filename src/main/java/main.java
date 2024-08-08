5452480fb382f2e645263b2121e3e1d9ae6a5290
hjw <2232285004@qq.com>
2024-08-08 22:26:05 +0800

    0-1背包问题

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,40 +1,43 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int M = sc.nextInt();
-        // int N = sc.nextInt();
-        //
-        // int[] preempt = new int[M];
-        // for (int i = 0; i < M; ++i) {
-        //     preempt[i] = sc.nextInt();
-        // }
-        //
-        // int[] value = new int[M];
-        // for (int i = 0; i < M; ++i) {
-        //     value[i] = sc.nextInt();
-        // }
+        int M = sc.nextInt();
+        int N = sc.nextInt();
 
-        int M = 6;
-        int N = 2;
-        int[] preempt = new int[]{2, 2, 3, 1, 5, 2};
-        int[] value = new int[]{2, 3, 1, 5, 4, 3};
+        int[] preempt = new int[M];
+        for (int i = 0; i < M; ++i) {
+            preempt[i] = sc.nextInt();
+        }
+
+        int[] value = new int[M];
+        for (int i = 0; i < M; ++i) {
+            value[i] = sc.nextInt();
+        }
+
+        // int M = 6;
+        // int N = 1;
+        // int[] preempt = new int[]{2, 2, 3, 1, 5, 2};
+        // int[] value = new int[]{2, 3, 1, 5, 4, 3};
 
 
-        int[][] dp = new int[N + 1][M + 1];
+        int[][] dp = new int[M][N + 1];
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i <= N; i++) {
             if (preempt[0] <= i) {
                 dp[0][i] = value[0];
             }
         }
         dp[0][0] = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j <= M; j++) {
+
+
+        for (int i = 1; i < M; i++) {
+            for (int j = 0; j <= N; j++) {
                 // 可以放入
                 if (preempt[i] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], value[i] + dp[i][j - preempt[j]]);
+                    dp[i][j] = Math.max(dp[i - 1][j], value[i] + dp[i - 1][j - preempt[i]]);
                 }
                 else {
                     // 如果当前物品放不进背包，则保持上一个物品的价值
@@ -43,6 +46,10 @@ public class main {
             }
         }
 
-        System.out.println(dp[N][M]);
+        for (int[] ints : dp) {
+            System.out.println(Arrays.toString(ints));
+        }
+
+        System.out.println(dp[M - 1][N]);
     }
 }
