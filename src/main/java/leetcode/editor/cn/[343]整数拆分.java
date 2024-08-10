56e253e57c675b31fb806d77e3fdb912156a1db9
hjw <2232285004@qq.com>
2024-08-10 22:35:32 +0800

    刷题

---
+++ src/main/java/leetcode/editor/cn/[343]整数拆分.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; ++i) {
+            for (int j = 1; j <= i - j; ++j) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
