fb493dd2e5ce72943585cec4c03505e6675474f7
hjw <2232285004@qq.com>
2024-08-15 22:41:00 +0800

    刷题

---
+++ src/main/java/suixianglu/ZeroAndOne_3维.java
@@ -0,0 +1,41 @@
+package suixianglu;
+
+/**
+ * @author qifei
+ * @since 2024-08-15
+ */
+public class ZeroAndOne_3维 {
+    public static void main(String[] args) {
+        String[] strs = new String[]{"10", "0001", "111001", "1", "0"};
+        new ZeroAndOne_3维().findMaxForm(strs, 5, 3);
+    }
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int length = strs.length;
+        int[][][] dp = new int[length + 1][m + 1][n + 1];
+        for (int i = 1; i <= length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+
+
+        return dp[length][m][n];
+    }
+
+    public int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
+}
