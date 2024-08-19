47a0f664447212249c1ccf625712428e660cedbf
hjw <2232285004@qq.com>
2024-08-19 22:30:01 +0800

    刷题

---
+++ src/main/java/leetcode/editor/cn/[96]不同的二叉搜索树.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numTrees(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // dp[n] = dp[n-i] + dp[i]
+        for (int i = 2; i <= n ; ++i) {
+            for (int j = 0; j < n ; ++j) {
+
+                dp[i] += dp[n - 1 - j] * dp[j];
+                System.out.println("i:" + i + " " + Arrays.toString(dp));
+
+            }
+        }
+        // System.out.println(Arrays.toString(dp));
+        return dp[n];
+
+    }
+}
