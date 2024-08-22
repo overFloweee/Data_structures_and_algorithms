ec3359d0a2581e966de3f39bdb5a4cb40094e42f
hjw <2232285004@qq.com>
2024-08-22 22:29:20 +0800

    刷题

--- src/main/java/leetcode/editor/cn/[70]爬楼梯.java
+++ src/main/java/leetcode/editor/cn/[70]爬楼梯.java
@@ -79,5 +79,18 @@ class Solution {
 
         return arr[n];
     }
+
+    // 再刷- 代码更简单了
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
 }
 // leetcode submit region end(Prohibit modification and deletion)
